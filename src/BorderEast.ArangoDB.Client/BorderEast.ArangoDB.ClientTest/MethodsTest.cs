ff7a59bf852874e62c7e2dc3fa4bc0f5a4337ff0
Andrew Grothe <agrothe@gmail.com>
2017-04-10 18:35:07 -0300

    bug fixes

--- src/BorderEast.ArangoDB.Client/BorderEast.ArangoDB.ClientTest/MethodsTest.cs
+++ src/BorderEast.ArangoDB.Client/BorderEast.ArangoDB.ClientTest/MethodsTest.cs
@@ -9,6 +9,7 @@ using Xunit;
 
 namespace BorderEast.ArangoDB.ClientTest
 {
+    
     public class MethodsTest
     {
         private ArangoClient client = ArangoClient.Client();
@@ -27,11 +28,51 @@ namespace BorderEast.ArangoDB.ClientTest
         public void AQLSelectEntity() {
 
 
-            client.SetDefaultDatabase(settings);
+            if (client.DB() == null) {
+                client.SetDefaultDatabase(settings);
+            }
 
             var user = client.DB().Query<User>("for u in User return u").ToList().Result.First();
 
             Assert.IsType<User>(user);
+
+        }
+
+        [Fact]
+        public void CRUDMethods() {
+            if (client.DB() == null) {
+                client.SetDefaultDatabase(settings);
+            }
+            client.InitDB(settings);
+
+            var guid = Guid.NewGuid().ToString();
+            var user = new User()
+            {
+                Password = "passcode",
+                Username = guid
+            };
+
+            var inserted = client.DB("client-test").InsertAsync(user).Result;
+
+            var insertUser = inserted.New;
+
+            Assert.IsType<User>(user);
+            Assert.Equal(guid, insertUser.Username);
+            Assert.Equal("passcode", insertUser.Password);
+
+            insertUser.Password = "1234";
+
+            var updated = client.DB("client-test").UpdateAsync(inserted.Key, insertUser).Result;
+            var updatedUser = updated.New;
+
+            Assert.IsType<User>(user);
+            Assert.Equal(inserted.Key, updated.Key);
+            Assert.Equal(guid, updatedUser.Username);
+            Assert.Equal("1234", updatedUser.Password);
+
+            Assert.True(client.DB("client-test").DeleteAsync<User>(updated.Key).Result);
+
+            Assert.Null(client.DB("client-test").GetByKeyAsync<User>(updated.Key).Result);
 
         }
 
